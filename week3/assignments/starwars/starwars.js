c0a6cc615bdad4cbe38ffe72d133c7795ebf3def
Tr vis <90364381+Shavu@users.noreply.github.com>
2022-02-13 16:37:01 -0700

    week3

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -1,25 +1,27 @@
 // declare any necessary variables
-let,
+let queryType, itemID;
 
-    // define a function called 'fetchData()' that passes the values from 
+// define a function called 'fetchData()' that passes the values from 
+// the 'queryType' and 'itemID' elements in starwars.html to the function 
+// called 'getFromSWAPI()'
 
 
-    // the 'queryType' and 'itemID' elements in starwars.html to the function 
-    // called 'getFromSWAPI()'
+function fetchData() {
+    queryType = document.querySelector("#queryType").value;
+    itemID = document.querySelector("#itemID").value;
+    // console.log(queryType, itemID);
+    getFromSWAPI(query, items);
+}
 
-    function getFromSWAPI() {
-        // assign values to any necessary variables
-        fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (data) {
-                updateInfo(data)
-            })
-            .catch(function (err) {
-                console.warn(err)
-            })
-    }
+
+function getFromSWAPI() {
+    // assign values to any necessary variables
+    fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
+        .then(response => response.json());
+        .then(data => updateInfo(data));
+        .catch (err => console.warn(err));
+}
+
 
 // create a new function called 'updateInfo()' that receives the data from 
 // the call to that function (see above). Use logic to write the appropriate
@@ -27,4 +29,11 @@ let,
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
 
-function updateInfo()
+function updateInfo(data) {
+    const keys = Object.keys(data);
+    document.querySelector("dataLabel1").innerText = keys[0];
+    document.querySelector("dataLabel2").innerText = keys[5];
+    document.querySelector("dataValue1").innerText = data[keys[0]];
+    document.querySelector("dataValue2").innerText = data[keys[5]];
+}
+   //Create if statement that checks the querytype (if the querytype if = to person)
